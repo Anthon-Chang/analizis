d259befa7d590673a8462758c055df54e7580fcd
Anthon-Chang <168015068+Anthon-Chang@users.noreply.github.com>
2025-10-08 10:29:16 -0500

    condicional.js

---
+++ condicional.js
@@ -0,0 +1,27 @@
+if ("+" === l[0])
+    break;
+
+if (k = k || " " !== m[0],
+    g.push(l),
+    h++,
+    "+" === m[0])
+    for (q = !0; "+" === m[0]; )
+        f.push(m),
+        m = c.lines[++c.index];
+
+l.substr(1) === m.substr(1) ? (f.push(m), c.index++) : q = !0;
+
+"+" === (b[h] || "")[0] && k && (q = !0);
+
+if (q)
+    c = f;
+else {
+    for (; h < b.length; )
+        g.push(b[h++]);
+    c = {
+        merged: g,
+        changes: f
+    }
+}
+
+c.merged ? (e = a.lines).push.apply(e, H(c.merged)) : ea(a, d ? c : b, d ? b : c);
